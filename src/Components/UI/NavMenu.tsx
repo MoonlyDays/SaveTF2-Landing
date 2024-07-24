1b175acbb774f0e91f4da214d4ed75df0a9e60d1
Daniil Basiuc-Brinzei <bioxdevcompany@gmail.com>
2024-07-24 16:37:23 +0300

    Update NavMenu.tsx

--- src/Components/UI/NavMenu.tsx
+++ src/Components/UI/NavMenu.tsx
@@ -2,25 +2,23 @@ import { LinkButton } from "../Button";
 
 export default function NavMenu() {
     return (
-        <>
-            <div className="sticky top-[-1px] z-50 h-full bg-[#1B1814]">
-                <nav className="bar-menu-top flex flex-row items-center justify-center gap-3">
-                    <LinkButton
-                        target="_self"
-                        href="#how-to-help"
-                        className={"justify-center py-1"}
-                    >
-                        How to help
-                    </LinkButton>
-                    <LinkButton
-                        target="_self"
-                        href="#faq"
-                        className={"justify-center py-1"}
-                    >
-                        FAQ
-                    </LinkButton>
-                </nav>
-            </div>
-        </>
+        <div className="sticky top-[-1px] z-50 h-full bg-[#1B1814]">
+            <nav className="bar-menu-top flex flex-row items-center justify-center gap-3">
+                <LinkButton
+                    target="_self"
+                    className={"justify-center py-1"}
+                    href="#how-to-help"
+                >
+                    How to help
+                </LinkButton>
+                <LinkButton
+                    target="_self"
+                    href="#faq"
+                    className={"justify-center py-1"}
+                >
+                    FAQ
+                </LinkButton>
+            </nav>
+        </div>
     );
 }
